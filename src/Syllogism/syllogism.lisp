399664a3c1d91155497e1a4dd0ee2998653e1a01
William Clifford <mr.william.clifford@gmail.com>
2014-11-04 23:23:11 -0800

    Syllogism: breakdown syllogisms into binary parts

---
+++ src/Syllogism/syllogism.lisp
@@ -0,0 +1,14 @@
+;;;; FILE: syllogism.lisp
+
+;;;; AUTHORS:
+;;;; William Clifford wc <wobh@yahoo.com>
+
+(in-package "CL-USER")
+
+(defpackage "ORG.WOBH.SYLLOGISM"
+  (:nicknames "SYLLOGISM")
+  (:documentation ""))
+
+(in-package "SYLLOGISM")
+
+(defun affirm ())
