4e0b8283902366cfe9df710f3356ff184bee7c63
William Clifford <wobh@yahoo.com>
2014-11-07 00:11:16 -0800

    spacetime WIP

--- src/spacetime-events.lisp
+++ src/spacetime-events.lisp
@@ -10,30 +10,93 @@
 
 ;;; Relativity, special
 
+(deftype speed ()
+  `(rational 0 1))
+
+(deftype range ()
+  `(rational 0))
+
 (defconstant +c+ 1/1
   "The speed of light")
 
+(define-condition superluminal (error)
+  ((object :reader object :initarg object))
+  (:report (lambda (condition stream)
+	     (format stream "Object ~A trying to break speed limit"
+		     (object condition)))))
+
+
+
+;;; 
+
+;;; basis (vector)
+;;; point
+;;; event
+
+;;; affine
+;;; origin
+;;; source
+;;; vertex
+;;; matrix
+
+;;; bearing
+;;; heading
+
+;;; displace
+;;; distance
+
+;;; position
+;;; velocity
+;;; location
+;;; interval
+
+;;; direction
+;;; magnitude
+;;; reference
+;;; spherical
+
+;;; coordinate
+
+;;; orientation
+;;; rectilinear
+;;; cylindrical
+
+(defclass position ()
+  (affine))
+
+(defmethod distance ((p1 position) (p2 position))
+  ())
+
 (defclass velocity ()
-  ((deltas :reader deltas :initarg :deltas))
-  (:documentation "Provides a slot `deltas' to hold a sequence of coordinate changes."))
+  (bases :reader bases :initarg :bases
+	 :documentation "basis vectors"))
+
+(defclass rectilinear-v (velocity)
+  ()
+  (:documentation "Velocities which may be mapped over array subscripts.
+
+This is a \"reverse Cartesian\" system (z, y, x) but should do what's
+expected if your space is modeled in a Lisp array of the same number
+of dimensions."))
+
+(defmethod initialize-instance :before ((v rectilinear-v) :bases bases)
+  (some (bases v)))
 
 (defun make-velocity (&rest deltas)
   (make-instance 'velocity :deltas deltas))
 
-(defmethod delta-t ((v velocity))
-  (first (deltas v)))
+(defun )
+
+
 
 (defmethod delta-z ((v velocity))
-  (second (deltas v)))
+  (first (deltas v)))
 
 (defmethod delta-y ((v velocity))
-  (third (deltas v)))
+  (second (deltas v)))
 
 (defmethod delta-x ((v velocity))
-  (fourth (deltas v)))
-
-(defmethod delta-n ((v velocity))
-  (rest ()))
+  (third (deltas v)))
 
 
 
